2a29d878a933d6fc99183d5decfbc5f4494451ad
Rifki Alhuraibi <riefky.alhuraibi@gmail.com>
2014-11-26 15:03:48 +0700

    add database pattern: metadata

---
+++ src/Database/Metadata/EntityMetadataInterface.php
@@ -0,0 +1,11 @@
+<?php namespace Veelasky\Foundry\Database\Metadata;
+/**
+ * Entity Metadata Interface
+ * 
+ * @author      veelasky <veelasky@gmail.com>
+ * @package     veelasky/foundry
+ */
+
+interface EntityMetadataInterface {
+
+} 

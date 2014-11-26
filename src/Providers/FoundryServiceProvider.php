cbbb789fb710a7bd3313cfb7e6144705a52552cc
Rifki Alhuraibi <riefky.alhuraibi@gmail.com>
2014-11-26 17:14:02 +0700

    add foundry service provider

---
+++ src/Providers/FoundryServiceProvider.php
@@ -0,0 +1,22 @@
+<?php namespace Veelasky\Foundry\Providers;
+/**
+ * Foundry Service Provider
+ * 
+ * @author      veelasky <veelasky@gmail.com>
+ * @package     veelasky/foundry
+ */
+
+use Illuminate\Support\ServiceProvider;
+
+class FoundryServiceProvider extends ServiceProvider {
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		// planning to do something awful
+	}
+} 

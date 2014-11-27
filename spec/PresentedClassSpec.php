20987d2a23ef9bff6524042e4952741ead5a24e1
Rifki Alhuraibi <riefky.alhuraibi@gmail.com>
2014-11-27 11:09:25 +0700

    refactoring spec folder

--- spec/PresentedClassSpec.php
+++ spec/PresentedClassSpec.php
@@ -1,17 +1,19 @@
 <?php namespace spec;
+/**
+ * Presented class specification
+ *
+ * @author      veelasky <veelasky@gmail.com>
+ * @package     veelasky/foundry
+ */
 
-use Illuminate\Database\Eloquent\Model;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
-use Veelasky\Foundry\Presenter\BasePresenter;
-use Veelasky\Foundry\Presenter\Factory\PresentableInterface;
-use Veelasky\Foundry\Presenter\Factory\PresentableTrait;
 
 class PresentedClassSpec extends ObjectBehavior
 {
     function let()
     {
-	    $this->beAnInstanceOf('spec\ModelStub');
+	    $this->beAnInstanceOf('spec\Stub\ModelStub');
     }
 
 	public function it_should_implement_presentable_interface()
@@ -21,7 +23,7 @@ class PresentedClassSpec extends ObjectBehavior
 
 	public function it_should_present_the_presenter_class()
 	{
-		$this->present()->shouldReturnAnInstanceOf('spec\PresenterStub');
+		$this->present()->shouldReturnAnInstanceOf('spec\Stub\PresenterStub');
 	}
 
 	public function it_should_resolve_parents_property_from_parent_class()
@@ -35,31 +37,4 @@ class PresentedClassSpec extends ObjectBehavior
 	{
 		$this->present()->getTable()->shouldReturn('table_name');
 	}
-}
-
-class ModelStub extends Model implements PresentableInterface {
-
-	use PresentableTrait;
-
-	protected $table = "table_name";
-
-	protected $guarded = false;
-
-	protected $attributes = [
-		'username'      => 'John Doe',
-		'email'         => 'john.doe@example.com'
-	];
-
-	/**
-	 * Get the full qualified class name for the presenter class
-	 *
-	 * @return string
-	 */
-	public function getPresenterClass() {
-		return 'spec\PresenterStub';
-	}
-}
-
-class PresenterStub extends BasePresenter {
-
 }
---
+++ spec/Stub/PresenterStub.php
@@ -0,0 +1,13 @@
+<?php namespace spec\Stub;
+/**
+ * Presenter stub for testing purpose
+ * 
+ * @author      veelasky <veelasky@gmail.com>
+ * @package     veelasky/foundry
+ */
+
+use Veelasky\Foundry\Presenter\BasePresenter;
+
+class PresenterStub extends BasePresenter {
+
+} 

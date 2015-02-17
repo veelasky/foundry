d31cfb7dd284fb4c35155185f06625b80dc95f54
Rifki Alhuraibi <riefky.alhuraibi@gmail.com>
2015-02-17 14:13:28 +0700

    start developing database repository

---
+++ src/Database/Repository/EloquentRepository.php
@@ -0,0 +1,13 @@
+<?php namespace Veelasky\Foundry\Database\Repository;
+/**
+ * Eloquent Repository
+ * 
+ * @author      veelasky <veelasky@gmail.com>
+ * @package     veelasky/foundry
+ */
+
+use Illuminate\Database\Eloquent\Model;
+
+class EloquentRepository {
+
+}

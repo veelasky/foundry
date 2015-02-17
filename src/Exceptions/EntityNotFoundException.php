1abc58e070cd3c36ca491a61e7f43a6376267f23
Rifki Alhuraibi <riefky.alhuraibi@gmail.com>
2015-02-17 15:11:32 +0700

    add eloquent repository

---
+++ src/Exceptions/EntityNotFoundException.php
@@ -0,0 +1,11 @@
+<?php namespace Veelasky\Foundry\Exceptions;
+/**
+ * Entity not found exception
+ * 
+ * @author      veelasky <veelasky@gmail.com>
+ * @package     veelasky/foundry
+ */
+
+use Exception;
+
+class EntityNotFoundException extends Exception{}
